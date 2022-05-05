1e804a6791e10c800617b6ba52e9deb93fe38271
Dhaesha M <dhaesha1@gmail.com>
2022-05-05 03:10:18 -0400

    setting up express app

--- server/index.js
+++ server/index.js
@@ -2,3 +2,10 @@ import { Express } from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+//initialize express server
+const app = express();
+//limit size of images
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
