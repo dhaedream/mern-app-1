9c3104f9a5008deb91cb5242576b9d56ef1456e6
Dhaesha M <dhaesha1@gmail.com>
2022-05-05 03:39:10 -0400

    setting up mongodb + mongoose environment

--- server/index.js
+++ server/index.js
@@ -11,3 +11,13 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // Mongodb cluod atlas
+const CONNECTION_URL =
+  "mongodb+srv://dhaedream:dhaedream@cluster0.cveob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
